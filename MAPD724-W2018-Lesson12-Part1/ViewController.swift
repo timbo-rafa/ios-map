7e448bb2b2784f4992256b0f6556b0e73af465ba
Student <student@setass-mac-mini-20.local>
2018-04-16 19:41:38 -0400

    Added MapKitView - closes #1

--- MAPD724-W2018-Lesson12-Part1/ViewController.swift
+++ MAPD724-W2018-Lesson12-Part1/ViewController.swift
@@ -1,25 +1,11 @@
-//
-//  ViewController.swift
-//  MAPD724-W2018-Lesson12-Part1
-//
-//  Created by Student on 2018-04-16.
-//  Copyright © 2018 Centennial College. All rights reserved.
-//
-
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
