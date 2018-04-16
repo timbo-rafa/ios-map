ec689b8df8d6b2c245f80b513af9af16cd602866
Student <student@setass-mac-mini-20.local>
2018-04-16 19:25:59 -0400

    Initial Commit

---
+++ MAPD724-W2018-Lesson12-Part1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPD724-W2018-Lesson12-Part1
+//
+//  Created by Student on 2018-04-16.
+//  Copyright © 2018 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MAPD724-W2018-Lesson12-Part1Tests/MAPD724_W2018_Lesson12_Part1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MAPD724_W2018_Lesson12_Part1Tests.swift
+//  MAPD724-W2018-Lesson12-Part1Tests
+//
+//  Created by Student on 2018-04-16.
+//  Copyright © 2018 Centennial College. All rights reserved.
+//
+
+import XCTest
+@testable import MAPD724_W2018_Lesson12_Part1
+
+class MAPD724_W2018_Lesson12_Part1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
