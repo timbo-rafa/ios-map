061d570af9a39f9d0885d9d24fd76195059c9a1a
Student <student@setass-mac-mini-20.local>
2018-04-16 20:08:03 -0400

    Toronto Default Map Location - closes #5

--- MAPD724-W2018-Lesson12-Part1/ViewController.swift
+++ MAPD724-W2018-Lesson12-Part1/ViewController.swift
@@ -2,9 +2,23 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var map: MKMapView!
 
+    let defaultLongitude = -79.3832
+    let defaultLatitude = 43.6532
+    let delta = 5.0
+    let mapLocation = CLLocationCoordinate2DMake(43.6532, -79.3832)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let loc = CLLocationCoordinate2DMake(defaultLatitude, defaultLongitude)
+        let span = MKCoordinateSpanMake(delta , delta)
+        let reg = MKCoordinateRegionMake(loc, span)
+        
+        self.map.region = reg
+        
     }
 
 }
