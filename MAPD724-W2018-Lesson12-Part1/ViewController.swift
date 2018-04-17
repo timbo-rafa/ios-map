1efeae6a959626ad1146ec17afbe16a8a4d33614
Student <student@setass-mac-mini-20.local>
2018-04-16 20:30:23 -0400

    made seeInMapApp button - closes #6

--- MAPD724-W2018-Lesson12-Part1/ViewController.swift
+++ MAPD724-W2018-Lesson12-Part1/ViewController.swift
@@ -25,6 +25,18 @@ class ViewController: UIViewController {
         ann.subtitle = "A place's that really cool"
         self.map.addAnnotation(ann)
     }
+    
+    @IBAction func seeInMapApp(_ sender: UIButton) {
+        let placemark = MKPlacemark(coordinate: self.mapLocation, addressDictionary: nil)
+        let mapItem = MKMapItem(placemark: placemark)
+        
+        mapItem.name = "A really icy place"
+        mapItem.openInMaps(launchOptions: [
+            MKLaunchOptionsMapTypeKey: MKMapType.standard.rawValue,
+            MKLaunchOptionsMapCenterKey: self.map.region.center,
+            MKLaunchOptionsMapSpanKey: self.map.region.span
+            ])
+    }
 
 }
 
